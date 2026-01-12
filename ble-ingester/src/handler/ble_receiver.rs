d6de597c3b0b993fc97c0a3108bf2276d92fe219
koyashiro <6698252+koyashiro@users.noreply.github.com>
2026-01-13 02:00:17 +0900

    fix(ble-ingester): prioritize btleplug over mock when both features enabled

--- ble-ingester/src/handler/ble_receiver.rs
+++ ble-ingester/src/handler/ble_receiver.rs
@@ -1,9 +1,9 @@
 #[cfg(feature = "btleplug")]
 mod btleplug_ble_receiver;
-#[cfg(feature = "mock")]
+#[cfg(all(feature = "mock", not(feature = "btleplug")))]
 mod mock_ble_receiver;
 
 #[cfg(feature = "btleplug")]
 pub use btleplug_ble_receiver::ble_receiver;
-#[cfg(feature = "mock")]
+#[cfg(all(feature = "mock", not(feature = "btleplug")))]
 pub use mock_ble_receiver::ble_receiver;
